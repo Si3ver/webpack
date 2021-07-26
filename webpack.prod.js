32524bbbfdcdfc5027a4969d76b55471bcdf475d
Si3ver <izhouwl@163.com>
2021-07-26 23:14:41 +0800

    feat: split dev & prod

---
+++ webpack.prod.js
@@ -0,0 +1,45 @@
+'use strict'
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    index: './src/index.js',
+    search: './src/search.js',
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: 'babel-loader',
+      },
+      {
+        test: /.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader']
+      },
+      {
+        test: /.(png|jpg|jpeg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10240, // 小于10KB的图片，打包进JS
+            }
+          }
+        ]
+      },
+      {
+        test: /.(woff|woff2|eot|ttf|otf)$/,
+        use: 'file-loader'
+      }
+    ]
+  }
+}
