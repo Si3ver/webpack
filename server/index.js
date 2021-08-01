48e709dbae40be9e4898e7e70687dd70e9cefc76
Si3ver <izhouwl@163.com>
2021-08-01 22:13:14 +0800

    feat: ssr打包首屏数据

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const path = require('path')
 const express = require('express')
 const { renderToString } = require('react-dom/server')
 const SSR = require('../dist/search-server')
+const data = require('./data.json')
 
 const template = fs.readFileSync(path.join(__dirname, '../dist/search.html'), 'utf-8')
 
@@ -29,5 +30,11 @@ const server = (port) => {
 server(process.env.PORT || 3000)
 
 const renderMarkup = (str) => {
-  return template.replace('<!--HTML_PLACEHOLDER-->', str)
+  const dataStr = JSON.stringify(data)
+  return template
+    .replace('<!--HTML_PLACEHOLDER-->', str)
+    .replace(
+      '<!--INITIAL_DATA_PLACEHOLDER-->',
+      `<script>window.__initial_data=${dataStr}</script>`
+    )
 }
